688b6addd1727bfd45562470e201e6dec3cd32af
Benjamin <sabean.ben@gmail.com>
2017-10-05 18:31:30 -0300

    Can generate tables based off devices table

--- DataBase/DbManager.py
+++ DataBase/DbManager.py
@@ -1,4 +1,5 @@
 import MySQLdb
+from DataEntry import DataEntry
 
 class DbManager:
     def __init__(self, myHost, myUser, myPasswd, myDb):
@@ -8,21 +9,40 @@ class DbManager:
 
         self.c=self.db.cursor()
         self.mydb = myDb
+        self.db = DataEntry(myHost, myUser, myPasswd, myDb)
         self.deviceTable = "Devices"
+        self.addressTable = "Addresses"
 
     def GetTables(self):
         try:
             self.c.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES "
                            + "WHERE TABLE_TYPE = 'BASE TABLE' "
                            + "AND TABLE_SCHEMA='" + self.mydb + "'")
-            return self.c.fetchall()
+            return self.c.fetchall()[0]
         except:
             pass
+        
+    def DecodeID(self, deviceID):
+        try:
+            self.c.execute("SELECT `Table Name` FROM `" + self.deviceTable + "` WHERE "
+                           + "`Device ID` = " + `deviceID`)
+            return self.c.fetchall()[0][0]
+        except:
+            pass      
 
     def GetDevices(self):
         try:
             self.c.execute("SELECT `Table Name` FROM `" + self.deviceTable + "`")
             return self.c.fetchall()
+        except:
+            pass
+        
+        
+    def GetSensorCount(self, deviceName):
+        try:
+            self.c.execute("SELECT Count(*) FROM `" + self.addressTable + "` WHERE "
+                           + "`Table Name` = \"" + deviceName + "\"")
+            return self.c.fetchall()[0][0]
         except:
             pass
 
@@ -31,17 +51,34 @@ class DbManager:
             return True
         else:
             return False
-'''
-    def createTable(self,tablename,db):
-        try:
-      
-'''      
-    def run(self):
-        #while True:
-        devices = self.GetDevices()
 
-        for row in devices:
-            if(!self.TableExists(self.GetTables(), row)):
-                
-      
+
+
+    def CreateTable(self,tableName, numSensors):
+        
+        query = "CREATE TABLE `" + tableName + "` (`TimeStamp` timestamp, "
+
+        for i in range(0, numSensors):
+            query += "`Sensor" + `(i+1)` + "` double,"
+        query = query[:-1]
+        query += ');'
+
+        print(query)
+
+        try:
+            self.c.execute(query)
+            return True
+        except:
+            return False
+
+    def InsertData(self, deviceID, numSensors, data):
+        table = self.DecodeID(deviceID)
+        if(not self.TableExists(self.GetTables(), table)):
+            self.CreateTable(table, numSensors)
+        if(self.db.insertData(table, data)):
+            return True
+        else:
+            return False
+         
+
     
